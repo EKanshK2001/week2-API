dde5bb2ce862a33a220f781ef3447c97da48d26d
Ekansh <efuforofficial@gmail.com>
2024-04-29 10:33:43 +0530

    implemented update route

--- src/controllers/updateController.ts
+++ src/controllers/updateController.ts
@@ -2,5 +2,12 @@ import { Request, Response } from 'express'
 import PageModel from '../db'
 
 export const updateOne = async (req: Request, res: Response) => {
+    //need to update the whole page object whenever called
+    //extract the body from req.body and update to db
 
+    const id = req.params.id;
+    const { title, quote, disclaimer, content } = req.body;
+
+    const updatedPage = await PageModel.findOneAndUpdate({_id: id}, {$set: {title, quote, disclaimer, content}}, {new: true});
+    res.json(updatedPage);
 }
--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,15 @@ const app:express.Application = express()
 app.use(express.json())
 
 
-app.get('/api/v1/', readAll);                                          //can use to display all kinds of pages (topics) (sends all the pages object)
+app.get('/api/v1/', readAll);                           //can use to display all kinds of pages (topics) (sends all the pages object)
 
-app.get('/api/v1/read/:id', readOne);                                  //read any specific page (id as param? yes. liked this better than query) (/read/2355732 vs /read?id=2355732)
+app.get('/api/v1/read/:id', readOne);                   //read any specific page (id as param? yes. liked this better than query) (/read/2355732 vs /read?id=2355732)
 
-app.post('/api/v1/create', createOne);                                 //create new page
+app.post('/api/v1/create', createOne);                  //create new page
 
-app.put('/api/v1/update:id', updateOne);                               //update any specific page with thier id as param
+app.put('/api/v1/update/:id', updateOne);               //update any specific page with thier id as param
 
-app.delete('/api/v1/delete:id', deleteOne);                            //delete any specific page
+app.delete('/api/v1/delete/:id', deleteOne);            //delete any specific page
 
 
 
